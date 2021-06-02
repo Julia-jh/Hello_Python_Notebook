9d4edc4254ee4e0eda0714831f7a2e8479d2d96e
Julia-jh <juliajhkim319@gmail.com>
2021-06-02 22:20:14 +0900

    new readme file

---
+++ Chapter_01/chapter_1_2.py
@@ -0,0 +1,37 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # 1.2.1 How to use Jupyter Notebook
+
+print('Hello, world!')
+
+print(1+1)
+print(2*5)
+print(10**3)
+
+# - press esc: to get out from editing mode
+# - press H: to see the list of Keyboard shortcuts
+
+# # 1.2.2 Python Basic
+
+
+
+# # 1.2.3 List and Dictionary
+
+# # 1.2.4 Conditional Statements and iteration
+
+# # 1.2.5 Function
+
+# # 1.2.6 Class and Instance
